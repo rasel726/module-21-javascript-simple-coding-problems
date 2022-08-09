49eacdffa63dd323505d1534c5a5007db13873a0
rasel726 <md.rasel2726@gmail.com>
2022-08-10 00:19:01 +0600

    max3 file added

---
+++ max3.js
@@ -0,0 +1,22 @@
+const jim = 69;
+const dela = 97;
+const chinku = 99;
+
+if(jim > dela && jim > chinku){
+    console.log('Jim will get the cake');
+}
+else if(dela > jim && dela > chinku){
+    console.log('Dela will eat cake in this bela');
+}
+else{
+    console.log('Chinku will get the cake');
+}
+
+Math.max(12, 45);
+
+// HOME WORK
+// write a function that will take 3 number will return the max number
+// write a function that will take 3 parameters and will return the min number
+
+// first time do it using if-else
+// second time do it using Math.min or Math.max
